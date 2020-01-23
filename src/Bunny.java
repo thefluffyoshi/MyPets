1628cc313c8d20bea0832584dffe68edfe6b38b7
thefluffyoshi <katielynntrinh@gmail.com>
2020-01-23 13:39:48 -0800

    Made Bunny and tried to instantiate Pet

--- src/Bunny.java
+++ src/Bunny.java
@@ -1,4 +1,15 @@
-public class Bunny
+public class Bunny extends Pet
 {
-    
-}
+    private String breed;
+    public Bunny (String name, String breed, int age, double weight)
+    {
+        super(name, age, weight);
+        this.breed = breed;
+    }//end constructor with super
+
+    public String toString()
+    {
+        String output = super.toString() + "\nBreed: " + breed;
+        return output;
+    }
+}//end Bunny
--- src/MyPetsDriver.java
+++ src/MyPetsDriver.java
@@ -4,8 +4,11 @@ public class MyPetsDriver
     {
         Dog Rex = new Dog("Rex", "German Shepherd", 2, 160.2);
         Lizard Mochi = new Lizard("Mochi", "Blue-Tongued Skink", 8, 2);
-
+        Bunny Apple = new Bunny("Apple", "Mini Lop", 3, 5);
+        //Pet Chris = new Pet("Chris", 15, 15.0); Pet is abstract class and cannot be instantiated
+        
         System.out.println(Rex);
         System.out.print(Mochi);
+        System.out.print(Apple);
     }
 }//end driver
